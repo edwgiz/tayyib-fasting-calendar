85540d28ed9b860f910bd9a1db6bc562d2ec7cd1
Eduard Gizatullin <edwgiz@gmail.com>
2026-05-21 22:57:02 +0300

    Adds bengali i18n bundle

--- i18n.js
+++ i18n.js
@@ -4,7 +4,7 @@ export async function i18n(path) {
   if (!i18nBundle) {
     const bcp47 = (navigator.languages && navigator.languages[0]) || navigator.language || 'en-US';
     let iso639_1 = bcp47.substring(0, 2);
-    if (!['en', 'ar'].includes(iso639_1)) {
+    if (!['en', 'ar', 'bn'].includes(iso639_1)) {
       iso639_1 = 'en';
     }
     i18nBundle = await (await fetch(`i18n/${iso639_1}.json`)).json();
