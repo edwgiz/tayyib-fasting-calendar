4a47f20e579f1423c054c95b3364a264287a40fa
Eduard Gizatullin <edwgiz@gmail.com>
2026-05-21 22:56:31 +0300

    I18n refactoring

--- i18n.js
+++ i18n.js
@@ -1,23 +1,18 @@
-let i18nCache;
-let i18nLocale = 'en';
+let i18nBundle;
 
 export async function i18n(path) {
-  if (!i18nCache) {
-    i18nCache = await (await fetch(`i18n/${i18nLocale}.json`)).json();
+  if (!i18nBundle) {
+    const bcp47 = (navigator.languages && navigator.languages[0]) || navigator.language || 'en-US';
+    let iso639_1 = bcp47.substring(0, 2);
+    if (!['en', 'ar'].includes(iso639_1)) {
+      iso639_1 = 'en';
+    }
+    i18nBundle = await (await fetch(`i18n/${iso639_1}.json`)).json();
   }
   if (path) {
-    const value = path.split('.').reduce((obj, key) => obj?.[key], i18nCache);
+    const value = path.split('.').reduce((obj, key) => obj?.[key], i18nBundle);
     return value ? value : path;
   } else {
-    return i18nCache;
-  }
-}
-
-export function setI18nLocale(locale) {
-  let simpleLocale = locale.substring(0, 2);
-  if (['en', 'ar'].includes(simpleLocale)) {
-    i18nLocale = simpleLocale;
-  } else {
-    i18nLocale = 'en';
+    return i18nBundle;
   }
 }
